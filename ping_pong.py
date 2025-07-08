2625220743e74df0a66cf872ec776a73d55dd666
AdviceForedoom29 <just.ugh@mail.ru>
2025-07-08 19:12:53 +0500

    v2
    Добавлена логика отбивания мяча от ракеток

--- ping_pong.py
+++ ping_pong.py
@@ -62,6 +62,15 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish != True:
+        window.fill(bg)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= -1
     
     display.update()
     clock.tick(FPS)
